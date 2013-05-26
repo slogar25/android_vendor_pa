9c4678c1c3c38a26879a9e024f1466eb0a5e5de8
dhacker29 <davidhackerdvm@gmail.com>
2013-05-25 23:22:25 -0500

    Add Motorola Razr HD (xt926)
    Change-Id: I4330276295022677779a5ddce47a66c0fbd61571

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -55,3 +55,6 @@ endif
 ifeq (pa_find5,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_find5.mk
 endif
+ifeq (pa_xt926,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_xt926.mk
+endif
---
+++ products/pa_xt926.mk
@@ -0,0 +1,51 @@
+# Copyright (C) 2012 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_xt926,$(TARGET_PRODUCT))
+
+# Define PA bootanimation size
+PARANOID_BOOTANIMATION_NAME := XHDPI
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_xhdpi
+
+# Build paprefs from sources
+PREFS_FROM_SOURCE ?= false
+
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/pa/config/cdma.mk)
+
+# Include ParanoidAndroid common configuration
+include vendor/pa/config/pa_common.mk
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/motorola/xt926/full_xt926.mk)
+
+# CM Package Extras
+-include vendor/pa/packages/cm.mk
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_xt926
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := XT926
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=XT926_verizon TARGET_DEVICE=XT926 BUILD_FINGERPRINT="motorola/XT926_verizon/vanquish:4.1.2/9.8.1Q-62_VQW_MR-2/6:user/release-keys"
+
+GET_VENDOR_PROPS := $(shell vendor/pa/tools/getvendorprops.py $(PRODUCT_NAME))
+GET_PROJECT_RMS := $(shell vendor/pa/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pa/tools/addprojects.py $(PRODUCT_NAME))
+
+endif
+
