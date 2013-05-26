0f5b732b4a376e26750f8e8f1821c19f461db48a
dhacker29 <davidhackerdvm@gmail.com>
2013-05-25 23:22:25 -0500

    Add Motorola HD (xt897c)
    Change-Id: I4330276295022677779a5ddce47a66c0fbd61571

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,6 +17,7 @@ add_lunch_combo pa_d710-userdebug
 add_lunch_combo pa_i605-userdebug
 add_lunch_combo pa_l900-userdebug
 add_lunch_combo pa_find5-userdebug
+add_lunch_combo pa_xt897c-userdebug
 add_lunch_combo pa_xt907-userdebug
 add_lunch_combo pa_xt925-userdebug
 add_lunch_combo pa_xt926-userdebug
